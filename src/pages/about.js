0aa5a791d3c541c303e1f00932ffd8a2eff1e90e
Arya Murthi <aryamurthi@Aryas-MBP.fios-router.home>
2020-04-05 17:14:08 -0400

    initial commit

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,17 +5,18 @@ import { Link } from 'gatsby'
 
 
 const AboutPage = ()=> {
-    return ( 
+    return( 
         <Layout>
             <h1>About</h1>
             <p>
-                Hi, I'm Arya. Lowkey just trying something out here maybe it'll turn out amazing but most likely it'll be a steaming pile of sh–
+                I'm a senior at Boston College studying business analytics and Marketing. 
+                <p>More importantly, I make a mean quesadilla.</p>
             </p>
             <p>
                  Feel free to <Link to="/contact">contact me.</Link>
             </p>
         </Layout>
             )
-        }   
+        }
 
 export default AboutPage
